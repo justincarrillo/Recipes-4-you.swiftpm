9552a11ba6225bb467ac56b6f724a205a9f80d53
justin <jcarrillo7045@stu.d214.org>
2024-05-01 14:20:34 -0500

    i made a new source file called dessert1 and addedthe recipe of ice cream cake

---
+++ Dessert1.swift
@@ -0,0 +1,18 @@
+//
+//  Dessert1.swift
+//  Recipes 4 you
+//
+//  Created by Justin Carrillo on 5/1/24.
+//
+
+import SwiftUI
+
+struct Dessert1: View {
+    var body: some View {
+        Text("Ingredients you need Cake mix: start with a store bought chocolate cake mix, add a cup fo water to the mix, three eggsadd richness and help bind the batter together, a neutral oil, such as vegetable oil, lends moisture without adding flavor, Store bought chocolate ice cream makes this cake recipe super convenient.")
+        
+    }
+}
+    
+    
+
