6fbf564aada0eb944db82f786bc65f06d598c49a
justin <jcarrillo7045@stu.d214.org>
2024-05-03 14:17:03 -0500

    i found tbhe imaghe of an ice cream cake

--- Dessert1.swift
+++ Dessert1.swift
@@ -9,7 +9,8 @@ import SwiftUI
 
 struct Dessert1: View {
     var body: some View {
-        Text("Ingredients you need Cake mix: start with a store bought chocolate cake mix, add a cup fo water to the mix, three eggsadd richness and help bind the batter together, a neutral oil, such as vegetable oil, lends moisture without adding flavor, Store bought chocolate ice cream makes this cake recipe super convenient.")
+        Text("Ingredients you need Cake mix: start with a store bought chocolate cake mix, add a cup fo water to the mix, three eggsadd richness and help bind the batter together, a neutral oil, such as vegetable oil, lends moisture without adding flavor, Store bought chocolate ice cream makes this cake recipe super convenient, and you are going to need oreos.")
+        Image("ice cream cake")
         
     }
 }
