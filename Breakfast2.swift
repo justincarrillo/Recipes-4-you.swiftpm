00f81708bc840012339b5145f74e3e1622ce52eb
Adam <ajakubiec7173@stu.d214.org>
2024-05-07 14:57:09 -0500

    redesigned breakfast recipe, made it look better/less bland

--- Breakfast2.swift
+++ Breakfast2.swift
@@ -8,23 +8,45 @@ import SwiftUI
 
 struct BreakFast2: View {
     var body: some View {
-        Text("Breakfast Burrito")
-            .font(.largeTitle)
-        Image("Breakfast2")
-        Text("Ingredients:")
-        
-        Text("Big Flour Tortillas \n Scrammbled Eggs\nSpicy potatoes (Roast potatoes with smoked paprika)\nAvacado\nPico de gallo\nHot sauce\nChopped Sausage\nother toppings like spinach or black beans")
-        
-        Text("1: Warm up the tortilla, scrammbled eggs, potatoes, and toppings that need to be warmed up ")
-        
-        Text("2. Put everything in a strip down the middle of the tortilla")
-        
-        Text("3. Add the other toppings that you like")
-        
-        Text("4. Roll up the burrito into burrito shape and then enjoy!")
+        VStack{
+            Text("Breakfast Burrito")
+                .font(.largeTitle)
+            Image("Breakfast2")
+            Text("Ingredients:")
+                .font(.headline)
+                .foregroundColor(.white)            
+                Text("- Big Flour Tortillas")
+                Text("- Scrammbled Eggs")
+                Text("- Spicy potatoes (Roast potatoes with smoked paprika)")
+                Text("- Avacado")
+                Text("- Pico de gallo")
+                Text("- Hot sauce")
+                Text("- Chopped Sausage")
+                Text("- other toppings like spinach or black beans")
+            
+            .foregroundColor(.white)
+            .frame(width: 500, height: 20)
+            
+            Text("Steps:")
+                .font(.headline)
+                .foregroundColor(.white)
+                .padding(.top, 10)
+            
+                Text("1. Warm up the tortilla, scrammbled eggs")
+                Text("potatoes, and toppings that need to be warmed up")
+                Text("2. Put everything in a strip down the middle of the tortilla.")
+                Text("3. Add the other toppings that you liked.")
+                Text("4. Roll up the burrito into burrito shape and then enjoy!")
+            
+            .foregroundColor(.white)
+        }
+        .padding()
+        .background(Color.cyan)
+        .cornerRadius(20)
+        .padding()
     }
+    
 }
-
 #Preview {
     BreakFast2()
 }
