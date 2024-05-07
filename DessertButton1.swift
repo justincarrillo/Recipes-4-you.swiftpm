de0abb53e931295050e5ed652a39bb39bcecb8d9
Aadi Bilfaqi <abilfaqi7040@stu.d214.org>
2024-05-07 14:49:04 -0500

    I added the text for the Ice Cream Cake recipe.

--- DessertButton1.swift
+++ DessertButton1.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct DessertButton1: View {
     var body: some View {
-        // Button(action: {
+        
         NavigationView{
             NavigationLink {
                 Dessert1()
@@ -20,10 +20,28 @@ struct DessertButton1: View {
                             .foregroundColor(.gray)
                             .frame(width: 300, height: 200)
                             .border(Color.black, width: 2)
-                        Image("ice cream cake")
-                            .resizable()
+                            
+                                           
+                        
                     }
+                    Divider()
+                    Text("Ice Cream Cake")
+                        .font(.headline)
+                        .padding(.top, 8)
+                        .foregroundColor(.black)
+                    
+                    
+                    Text("Calories: 413")
+                        .font(.subheadline)
+                        .foregroundColor(.black)
+
+                    
                 }
+                .padding(8)
+                .background(Color.cyan)
+                .cornerRadius(10)
+                .shadow(radius: 5)
+                .border(Color.white, width: 2)
             }
         }
     }
