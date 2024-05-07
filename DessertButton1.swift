ebb72f9bebcaf270bc8022f33a99b49fc5b8c8d7
justin <jcarrillo7045@stu.d214.org>
2024-05-07 14:12:36 -0500

    i made a navigation link to the buttonand dessert 1

--- DessertButton1.swift
+++ DessertButton1.swift
@@ -9,6 +9,22 @@ import SwiftUI
 
 struct DessertButton1: View {
     var body: some View {
-        Text("Placeholder")
+        // Button(action: {
+        NavigationView{
+            NavigationLink {
+                Dessert1()
+            } label: {
+                VStack {
+                    ZStack {
+                        Rectangle()
+                            .foregroundColor(.gray)
+                            .frame(width: 300, height: 200)
+                            .border(Color.black, width: 2)
+                        Image("ice cream cake")
+                            .resizable()
+                    }
+                }
+            }
+        }
     }
 }
