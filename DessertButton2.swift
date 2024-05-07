e5c414a03ba79820951264f8162dc91c1caccd79
Santo <smathew7247@stu.d214.org>
2024-05-07 13:57:39 -0500

    made new file called dessertButton2

---
+++ DessertButton2.swift
@@ -0,0 +1,8 @@
+//
+//  DessertButton2.swift
+//  Recipes 4 you
+//
+//  Created by Santo Mathew on 5/7/24.
+//
+
+import Foundation
--- dessert2.swift
+++ dessert2.swift
@@ -20,9 +20,12 @@ struct dessert2: View {
             
             
             Text("Ingredients:\n14 tbsp of Unsalted Butter\n11 1/4 cups of dark chocolate chips\n1 cup of brown sugar\n 3 eggs\n1 tsp of vanilla extract\n 1/2 cup of plain flour\n1/4 cup of cocoa powder\npinch of salt\n1.5 cups of roughly chopped nut (optional)")
+            
             Text("Steps:")
                 .font(.largeTitle)
+            
             Text("Preheat oven to 350ºf\nSpray an 8 inch square tin with oil and line it with baking/parchment paper with overhang\n Place butter and chocolate chips in a heaetproof bowl, microwave in 30 second bursts until it is melted and stir until smooth\nAdd sugar and vanilla and  mix until smooth and molten.\nAdd flour, cocoa and salt and stir until smooth. Stir in chopped chocolate, pour into pan.\n Bake 24 minutes for really gooey in the centre, 28 minutes for fudgey but still very moist, 32 minutes for moist fudge calke like\n if you didn't use the extra chocolate for stirring in reduce cook time by 2 minutes.\n Rest for 10 minutes before lifting out of pan. Allow to cool before eating.")
+            
             Image("Dessert2")
             
             
