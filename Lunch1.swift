68d382ad4549d6fe77460d93a1a9bdd617206d72
justin <jcarrillo7045@stu.d214.org>
2024-05-13 14:07:10 -0500

    i added text/ steps for chicken tendys

--- Lunch1.swift
+++ Lunch1.swift
@@ -12,7 +12,7 @@ struct Lunch1: View {
         Text("Chicken Tenders")
         Image("Lunch1")
         Text("How to make chicken tenders, the ingredients, 1 lb chicken tenderloins, 1 egg, 2 C all purpose flour, 1 C cornstach, baking powder, buttermilk, 2 tbsp hot sauce, 3 tsp seasoned salt, 1 tsp powdered sugar, 2 tsp paprika, 2 tsp granulated garlic, 1/2 tsp cayenne pepper, 1 tsp cracked pepper, oil for frying")
-        Text("Heat oven to 425 f. line cookie sheet with foil; spary with cooking spary, in shallow dish, place flour. in another dish, beat  egg and water. in third shallow dish, mix bread crumbs and cheese. coat chicken with flour; dip into egg mixture, then coat with bread crumbs mixture. plcae on cookie sheet. bake 15 to 20 minutes, turning once, untill chicken is no longer pink in center and coating is golden brpwn. serve with dipping sauce.")
+        Text("Heat oven to 425 f. Line cookie sheet with foil; spary with cooking spary, In shallow dish, place flour. In another dish, beat  egg and water. in third shallow dish, mix bread crumbs and cheese. Coat chicken with flour; dip into egg mixture, then coat with bread crumbs mixture. Plcae on cookie sheet. Bake 15 to 20 minutes, turning once, untill chicken is no longer pink in center and coating is golden brpwn. Serve with dipping sauce.")
         
         
     }
