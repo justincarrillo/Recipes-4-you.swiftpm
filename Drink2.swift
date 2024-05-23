46896011e6ad85610d31be7c677e3b0dcd5dedda
justin <jcarrillo7045@stu.d214.org>
2024-05-23 14:23:00 -0500

    deleted the protein smoothie and added a new recipe horchata.

--- Drink2.swift
+++ Drink2.swift
@@ -3,33 +3,8 @@ import SwiftUI
 struct Drink2: View {
     var body: some View {
         VStack{
-            Text("Protein Smoothie")
-                .font(.largeTitle)
-            Image("Smoothie")
-                .resizable()
-                .frame(width: 300, height: 200)
-            Text("Ingredients:")
-                .font(.headline)
-                .foregroundColor(.white)
-            Text("- 1 cup of frozen fruit (if it is not frozen just add ice to the blend as well.)")
-            Text("- about  1/2 a cup of milk (or water)")
-            Text("- 2 scoops of protein powder (I reccommend PB fit because it adds a peanut buttery taste to it.) ")
-            Text("- about 20 cashews")
-            Text("- 1/4 cup of greek yogurt for extra protein")
-            Text("- 1 tbsp of maple  syrup for a sweeter smoothie (optional)")
-                .foregroundColor(.white)
-                .frame(width: 500, height: 20)
-            
-            Text("Steps:")
-                .font(.headline)
-                .foregroundColor(.white)
-                .padding(.top, 10)
-            
-            Text("1. Put the frozen fruit in the blender along with the cashews, milk (or water), and the protein powder")
-            Text("2. After blending for about 30 seconds, add the syrup and greek yogurt.")
-            Text("3. Blend until smooth and then serve and enjoy!")
-            Text("Tip. the more milk you add the runnier it will be.")
-                .foregroundColor(.white)
+            Text("use q cup of any uncooked white rice and 2 cinnamon sticks. the milk base comes from 1 can each of evaporated milk and condensed milk. this what gives the horchata the wonderful flavor and a creamy consistency. if the condensed milk does not make it sweet enough for you, you can add more sugar to taste. ground cinnamon to garnish(optional). 1/2 teaspoon vanilla (optional)- just dont use too much vanilla as we dont want to overpower the cinnamon and rice flavor. ")
+            Text("Steps: Wash and drain the rice. Place the rice, cinnamon stick and 4 cups of water into a bowl. cover the bowl and refrigerate overnight or a minimum of 4 hours. Add 1/2 of the rice and 1/2 of the soaking water and some of the cinnamon stick in a blender. puree untill its very smooth and forms a paste like texture. about 4 minutes long. using a very fine strainer pour the mixture over a pitcher  starin out as much liquid as possible, pushing on the solids with a spatuala or spoon. repeat this process for the rest of the rice and cinnamon mixture. stir in the milks, vanilla and 4 cups of water. stir well. taste and add more sugar or water if needed. chill and stir before serving over ice. ")
         }
         .padding()
         .background(Color.orange)
